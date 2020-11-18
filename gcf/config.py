3cf7a4479d832cbad6748507854b89bd53c0b331
yamada-masa <m_yamada0526@yahoo.co.jp>
2020-11-18 11:14:49 +0900

    ignore

--- gcf/config.py
+++
@@ -1,8 +0,0 @@
-PROJECT = "xxxxxx"
-ZONE = "xxxxxx"
-INSTANCE_NAME = "xxxxxx"
-AUTH_KEY = "xxxxxxx"
-USER_ID = [
-    "amzn1.ask.account.xxxxxxxxxxxxxxxx",
-    "amzn1.ask.account.yyyyyyyyyyyyyyyy",
-]
